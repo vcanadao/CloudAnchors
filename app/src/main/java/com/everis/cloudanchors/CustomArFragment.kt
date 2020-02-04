73134c2ba6a8d859c072bca21a8ce4e95440b5cd
Víctor Manuel Cañada Ojeda <victorcanoje@gmail.com>
2020-02-04 22:15:14 +0100

    Creamos nuestro ArFragment customizado. Cambiamos la configuracion para no mostrar la animacion de la mano al inicio y habilitar el modo CloudAnchor

---
+++ app/src/main/java/com/everis/cloudanchors/CustomArFragment.kt
@@ -0,0 +1,21 @@
+package com.everis.cloudanchors
+
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.sceneform.ux.ArFragment
+
+class CustomArFragment : ArFragment(){
+
+    override fun getSessionConfiguration(session: Session?): Config {
+     //Deshabilitamos la animacion de la mano del inicio
+        planeDiscoveryController.setInstructionView(null)
+
+        //Obtenemos la configuracion por defecto llamando al metodo getSessionConfiguration de la clase Padre
+        val config = super.getSessionConfiguration(session)
+
+        //Habilitamos el modo CloudAnchor
+        config.cloudAnchorMode = Config.CloudAnchorMode.ENABLED
+
+        return config
+    }
+}
